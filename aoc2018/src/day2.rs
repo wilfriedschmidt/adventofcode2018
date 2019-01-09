1b1b7c4a41766da1c802741538e2f28cf67a49ae
korben dallas <wtschmidt@gmail.com>
2019-01-08 20:26:33 -0800

    day2

---
+++ aoc2018/src/day2.rs
@@ -0,0 +1,88 @@
+use std::collections::HashMap;
+use util::*;
+
+pub fn str_diff(str1:&str, str2:&str) -> (i32, usize)
+{
+  let mut diff=0;
+  let mut index=0;
+
+  for i in 0..str1.len()
+  {
+    if str1.chars().nth(i) != str2.chars().nth(i)
+    {
+      diff += 1;
+      index = i;
+    }   
+  }
+
+  return (diff, index);
+}
+
+pub fn go(filename:&str)
+{
+  let payload:Vec<u8> = readfile(filename);
+  let payloadstr:String = String::from_utf8(payload).unwrap();
+  let lines:Vec<&str> = payloadstr.split('\n').collect();
+
+  let mut twos = 0;
+  let mut threes = 0;
+
+  for i in 0..lines.len()
+  {
+    let chars:Vec<char> = lines[i].chars().collect();
+    let mut mymap = HashMap::new();
+    
+    for j in 0..chars.len()
+    {
+      if mymap.contains_key(&chars[j])
+      {
+        *mymap.get_mut(&chars[j]).unwrap() += 1;
+      }
+      else
+      {
+        mymap.insert(chars[j],1);
+      }
+    }
+
+    let mut two = false;
+    let mut three = false;
+
+    for (_key, value) in mymap
+    {
+      if value==2
+      {
+        two = true;
+      }
+      if value==3
+      {
+        three = true;
+      }
+    }
+
+    if two
+    {
+      twos+=1;
+    }
+    if three
+    {
+      threes+=1;
+    }
+  }
+
+  println!("checksum {}", twos*threes);
+
+  for i in 0..lines.len()
+  {
+    for j in i..lines.len()
+    {
+      if lines[j].len() > 1
+      {
+        let result = str_diff( lines[i], lines[j] );
+        if result.0 == 1
+        {
+          println!("{}{}", &lines[i][..result.1], &lines[i][result.1+1..] );
+        }
+      }
+    }
+  }
+}
